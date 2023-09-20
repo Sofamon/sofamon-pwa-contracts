b409087c4b6ea476288c542dda111e8f9c7ff4c4
XingyuLi20 <lixingyu@berkeley.edu>
2023-09-20 18:11:26 -0400

    init: sofamon shares contract v1

---
+++ src/LibString.sol
@@ -0,0 +1,186 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+/// @notice Library for converting numbers into strings and other string operations.
+/// @author Solady (https://github.com/vectorized/solady/blob/main/src/utils/LibString.sol)
+/// @author Modified from Solmate (https://github.com/transmissions11/solmate/blob/main/src/utils/LibString.sol)
+library LibString {
+    /// @dev Returns the base 10 decimal representation of `value`.
+    function toString(uint256 value) internal pure returns (string memory str) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            // The maximum value of a uint256 contains 78 digits (1 byte per digit), but
+            // we allocate 0xa0 bytes to keep the free memory pointer 32-byte word aligned.
+            // We will need 1 word for the trailing zeros padding, 1 word for the length,
+            // and 3 words for a maximum of 78 digits.
+            str := add(mload(0x40), 0x80)
+            // Update the free memory pointer to allocate.
+            mstore(0x40, add(str, 0x20))
+            // Zeroize the slot after the string.
+            mstore(str, 0)
+
+            // Cache the end of the memory to calculate the length later.
+            let end := str
+
+            let w := not(0) // Tsk.
+            // We write the string from rightmost digit to leftmost digit.
+            // The following is essentially a do-while loop that also handles the zero case.
+            for {
+                let temp := value
+            } 1 {
+
+            } {
+                str := add(str, w) // `sub(str, 1)`.
+                // Write the character to the pointer.
+                // The ASCII index of the '0' character is 48.
+                mstore8(str, add(48, mod(temp, 10)))
+                // Keep dividing `temp` until zero.
+                temp := div(temp, 10)
+                if iszero(temp) {
+                    break
+                }
+            }
+
+            let length := sub(end, str)
+            // Move the pointer 32 bytes leftwards to make room for the length.
+            str := sub(str, 0x20)
+            // Store the length.
+            mstore(str, length)
+        }
+    }
+
+    /// @dev Returns the hexadecimal representation of `value`,
+    /// left-padded to an input length of `length` bytes.
+    /// The output is prefixed with "0x" encoded using 2 hexadecimal digits per byte,
+    /// giving a total length of `length * 2 + 2` bytes.
+    /// Reverts if `length` is too small for the output to contain all the digits.
+    function toHexString(
+        uint256 value,
+        uint256 length
+    ) internal pure returns (string memory str) {
+        str = toHexStringNoPrefix(value, length);
+        /// @solidity memory-safe-assembly
+        assembly {
+            let strLength := add(mload(str), 2) // Compute the length.
+            mstore(str, 0x3078) // Write the "0x" prefix.
+            str := sub(str, 2) // Move the pointer.
+            mstore(str, strLength) // Write the length.
+        }
+    }
+
+    /// @dev Returns the hexadecimal representation of `value`,
+    /// left-padded to an input length of `length` bytes.
+    /// The output is prefixed with "0x" encoded using 2 hexadecimal digits per byte,
+    /// giving a total length of `length * 2` bytes.
+    /// Reverts if `length` is too small for the output to contain all the digits.
+    function toHexStringNoPrefix(
+        uint256 value,
+        uint256 length
+    ) internal pure returns (string memory str) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            // We need 0x20 bytes for the trailing zeros padding, `length * 2` bytes
+            // for the digits, 0x02 bytes for the prefix, and 0x20 bytes for the length.
+            // We add 0x20 to the total and round down to a multiple of 0x20.
+            // (0x20 + 0x20 + 0x02 + 0x20) = 0x62.
+            str := add(mload(0x40), and(add(shl(1, length), 0x42), not(0x1f)))
+            // Allocate the memory.
+            mstore(0x40, add(str, 0x20))
+            // Zeroize the slot after the string.
+            mstore(str, 0)
+
+            // Cache the end to calculate the length later.
+            let end := str
+            // Store "0123456789abcdef" in scratch space.
+            mstore(0x0f, 0x30313233343536373839616263646566)
+
+            let start := sub(str, add(length, length))
+            let w := not(1) // Tsk.
+            let temp := value
+            // We write the string from rightmost digit to leftmost digit.
+            // The following is essentially a do-while loop that also handles the zero case.
+            for {
+
+            } 1 {
+
+            } {
+                str := add(str, w) // `sub(str, 2)`.
+                mstore8(add(str, 1), mload(and(temp, 15)))
+                mstore8(str, mload(and(shr(4, temp), 15)))
+                temp := shr(8, temp)
+                if iszero(xor(str, start)) {
+                    break
+                }
+            }
+
+            if temp {
+                // Store the function selector of `HexLengthInsufficient()`.
+                mstore(0x00, 0x2194895a)
+                // Revert with (offset, size).
+                revert(0x1c, 0x04)
+            }
+
+            // Compute the string's length.
+            let strLength := sub(end, str)
+            // Move the pointer and write the length.
+            str := sub(str, 0x20)
+            mstore(str, strLength)
+        }
+    }
+
+    /**
+     * @dev Converts an `address` with fixed length of 20 bytes to its not checksummed ASCII `string` hexadecimal representation.
+     */
+    function toString(address addr) internal pure returns (string memory) {
+        return toHexString(uint256(uint160(addr)), 20);
+    }
+
+    /// @dev Returns a concatenated string of `a` and `b`.
+    /// Cheaper than `string.concat()` and does not de-align the free memory pointer.
+    function concat(
+        string memory a,
+        string memory b
+    ) internal pure returns (string memory result) {
+        /// @solidity memory-safe-assembly
+        assembly {
+            let w := not(0x1f)
+            result := mload(0x40)
+            let aLength := mload(a)
+            // Copy `a` one word at a time, backwards.
+            for {
+                let o := and(add(aLength, 0x20), w)
+            } 1 {
+
+            } {
+                mstore(add(result, o), mload(add(a, o)))
+                o := add(o, w) // `sub(o, 0x20)`.
+                if iszero(o) {
+                    break
+                }
+            }
+            let bLength := mload(b)
+            let output := add(result, aLength)
+            // Copy `b` one word at a time, backwards.
+            for {
+                let o := and(add(bLength, 0x20), w)
+            } 1 {
+
+            } {
+                mstore(add(output, o), mload(add(b, o)))
+                o := add(o, w) // `sub(o, 0x20)`.
+                if iszero(o) {
+                    break
+                }
+            }
+            let totalLength := add(aLength, bLength)
+            let last := add(add(result, 0x20), totalLength)
+            // Zeroize the slot after the string.
+            mstore(last, 0)
+            // Stores the length.
+            mstore(result, totalLength)
+            // Allocate memory for the length and the bytes,
+            // rounded up to a multiple of 32.
+            mstore(0x40, and(add(last, 0x1f), w))
+        }
+    }
+}
---
+++ src/SofamonShares.sol
@@ -0,0 +1,257 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Ownable} from "./Ownable.sol";
+import {LibString} from "./LibString.sol";
+
+// TODO: Events, final pricing model, revenue sharing
+
+contract SofamonShares is Ownable {
+    address public protocolFeeDestination;
+    uint256 public protocolFeePercent;
+    uint256 public subjectFeePercent;
+    uint256 public holderFeePercent;
+    string public baseMetadataURI;
+
+    uint256 public thresholdSupply;
+
+    event Trade(
+        address trader,
+        address subject,
+        bool isBuy,
+        uint256 shareId,
+        uint256 shareAmount,
+        uint256 ethAmount,
+        uint256 protocolEthAmount,
+        uint256 holderEthAmount,
+        uint256 subjectEthAmount,
+        uint256 supply
+    );
+
+    struct Holder {
+        address holderAddress;
+        uint256 shareBalance;
+    }
+
+    // The supply of the current share for a specific holder
+    // SharesSubject => (ID => (Holder => Balance))
+    mapping(address => mapping(uint256 => mapping(address => uint256)))
+        public sharesBalance;
+
+    // The total supply of the current share
+    // SharesSubject => (ID => Supply)
+    mapping(address => mapping(uint256 => uint256)) public sharesSupply;
+
+    // Mapping to store holders for each share subject
+    // SharesSubject => (ID => Holder)
+    mapping(address => mapping(uint256 => Holder[])) public shareHolders;
+
+    function setURI(string memory _baseMetadataURI) public onlyOwner {
+        baseMetadataURI = _baseMetadataURI;
+    }
+
+    function shareURI(
+        address sharesSubject,
+        uint256 shareId
+    ) public view returns (string memory) {
+        string memory baseMetadataURI_ = LibString.concat(
+            baseMetadataURI,
+            LibString.toString(sharesSubject)
+        );
+        return LibString.concat(baseMetadataURI_, LibString.toString(shareId));
+    }
+
+    function setFeeDestination(address _feeDestination) public onlyOwner {
+        protocolFeeDestination = _feeDestination;
+    }
+
+    function setProtocolFeePercent(uint256 _feePercent) public onlyOwner {
+        protocolFeePercent = _feePercent;
+    }
+
+    function setSubjectFeePercent(uint256 _feePercent) public onlyOwner {
+        subjectFeePercent = _feePercent;
+    }
+
+    function setHolderFeePercent(uint256 _feePercent) public onlyOwner {
+        holderFeePercent = _feePercent;
+    }
+
+    function setThresholdSupply(uint256 _threshold) public onlyOwner {
+        thresholdSupply = _threshold;
+    }
+
+    function getPrice(
+        uint256 supply,
+        uint256 amount
+    ) public pure returns (uint256) {
+        uint256 sum1 = supply == 0
+            ? 0
+            : ((supply - 1) * (supply) * (2 * (supply - 1) + 1)) / 6;
+        uint256 sum2 = supply == 0 && amount == 1
+            ? 0
+            : ((supply - 1 + amount) *
+                (supply + amount) *
+                (2 * (supply - 1 + amount) + 1)) / 6;
+        uint256 summation = sum2 - sum1;
+        return (summation * 1 ether) / 16000;
+    }
+
+    function getBuyPrice(
+        address sharesSubject,
+        uint256 shareId,
+        uint256 amount
+    ) public view returns (uint256) {
+        return getPrice(sharesSupply[sharesSubject][shareId], amount);
+    }
+
+    function getSellPrice(
+        address sharesSubject,
+        uint256 shareId,
+        uint256 amount
+    ) public view returns (uint256) {
+        return getPrice(sharesSupply[sharesSubject][shareId] - amount, amount);
+    }
+
+    function getBuyPriceAfterFee(
+        address sharesSubject,
+        uint256 shareId,
+        uint256 amount
+    ) public view returns (uint256) {
+        uint256 price = getBuyPrice(sharesSubject, shareId, amount);
+        uint256 protocolFee = (price * protocolFeePercent) / 1 ether;
+        uint256 subjectFee = (price * subjectFeePercent) / 1 ether;
+        uint256 holderFee = (price * holderFeePercent) / 1 ether;
+        return price + protocolFee + subjectFee + holderFee;
+    }
+
+    function getSellPriceAfterFee(
+        address sharesSubject,
+        uint256 shareId,
+        uint256 amount
+    ) public view returns (uint256) {
+        uint256 price = getSellPrice(sharesSubject, shareId, amount);
+        uint256 protocolFee = (price * protocolFeePercent) / 1 ether;
+        uint256 subjectFee = (price * subjectFeePercent) / 1 ether;
+        uint256 holderFee = (price * holderFeePercent) / 1 ether;
+        return price - protocolFee - subjectFee - holderFee;
+    }
+
+    // Function to distribute the holder's fee among all holders
+    function distributeHolderFee(
+        address sharesSubject,
+        uint256 shareId,
+        uint256 holderFee
+    ) internal {
+        Holder[] storage holders = shareHolders[sharesSubject][shareId];
+        uint256 totalShares = sharesSupply[sharesSubject][shareId];
+
+        // Distribute the holder's fee proportionally to each holder
+        for (uint256 i = 0; i < holders.length; i++) {
+            uint256 feeShare = (holders[i].shareBalance * holderFee) /
+                totalShares;
+            (bool success, ) = holders[i].holderAddress.call{value: feeShare}(
+                ""
+            );
+            require(success, "Failed to distribute holder's fee");
+        }
+    }
+
+    function buyShares(
+        address sharesSubject,
+        uint256 shareId,
+        uint256 amount
+    ) public payable {
+        uint256 supply = sharesSupply[sharesSubject][shareId];
+        uint256 count = sharesBalance[sharesSubject][shareId][msg.sender];
+        require(
+            supply > 0 || sharesSubject == msg.sender,
+            "Only the shares' subject can buy the first share"
+        );
+
+        // Check if the total supply is below the threshold, and if so, limit the user to buying 1 share
+        if (supply < thresholdSupply) {
+            require(
+                amount == 1 && count == 0,
+                "You can only buy 1 share when the supply is below the threshold, and you don't already own shares"
+            );
+        }
+
+        uint256 price = getPrice(supply, amount);
+        uint256 protocolFee = (price * protocolFeePercent) / 1 ether;
+        uint256 subjectFee = (price * subjectFeePercent) / 1 ether;
+        uint256 holderFee = (price * holderFeePercent) / 1 ether;
+        require(
+            msg.value >= price + protocolFee + subjectFee,
+            "Insufficient payment"
+        );
+        sharesBalance[sharesSubject][shareId][msg.sender] =
+            sharesBalance[sharesSubject][shareId][msg.sender] +
+            amount;
+        sharesSupply[sharesSubject][shareId] = supply + amount;
+        emit Trade(
+            msg.sender,
+            sharesSubject,
+            true,
+            shareId,
+            amount,
+            price,
+            protocolFee,
+            subjectFee,
+            holderFee,
+            supply + amount
+        );
+        (bool success1, ) = protocolFeeDestination.call{value: protocolFee}("");
+        (bool success2, ) = sharesSubject.call{value: subjectFee}("");
+        // Distribute holder's fee among all holders
+        distributeHolderFee(sharesSubject, shareId, holderFee);
+        bool success3 = true;
+        require(success1 && success2 && success3, "Unable to send funds");
+    }
+
+    // TODO: distribute fees to holders
+    function sellShares(
+        address sharesSubject,
+        uint256 shareId,
+        uint256 amount
+    ) public payable {
+        uint256 supply = sharesSupply[sharesSubject][shareId];
+        require(supply > amount, "Cannot sell the last share");
+        uint256 price = getPrice(supply - amount, amount);
+        uint256 protocolFee = (price * protocolFeePercent) / 1 ether;
+        uint256 subjectFee = (price * subjectFeePercent) / 1 ether;
+        uint256 holderFee = (price * holderFeePercent) / 1 ether;
+        require(
+            sharesBalance[sharesSubject][shareId][msg.sender] >= amount,
+            "Insufficient shares"
+        );
+        sharesBalance[sharesSubject][shareId][msg.sender] =
+            sharesBalance[sharesSubject][shareId][msg.sender] -
+            amount;
+        sharesSupply[sharesSubject][shareId] = supply - amount;
+        emit Trade(
+            msg.sender,
+            sharesSubject,
+            false,
+            shareId,
+            amount,
+            price,
+            protocolFee,
+            subjectFee,
+            holderFee,
+            supply - amount
+        );
+        (bool success1, ) = msg.sender.call{
+            value: price - protocolFee - subjectFee
+        }("");
+        (bool success2, ) = protocolFeeDestination.call{value: protocolFee}("");
+        (bool success3, ) = sharesSubject.call{value: subjectFee}("");
+        // Distribute holder's fee among all holders
+        distributeHolderFee(sharesSubject, shareId, holderFee);
+        bool success4;
+        require(
+            success1 && success2 && success3 && success4,
+            "Unable to send funds"
+        );
+    }
+}

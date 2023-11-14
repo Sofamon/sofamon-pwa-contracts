5d434c5f27924cceff37f6595c4651cd306d2e4e
XingyuLi20 <lixingyu@berkeley.edu>
2023-11-14 12:32:44 -0800

    update: version

--- src/Context.sol
+++ src/Context.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.17;
 
 /**
  * @dev Provides information about the current execution context, including the
--- src/Ownable.sol
+++ src/Ownable.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.17;
 
 import {Context} from "./Context.sol";
 
